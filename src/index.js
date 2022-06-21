8604b9a119f7c865b8fb00f022d39ac20b8ccd98
Daniil Ognevoy <roxxor503@gmail.com>
2022-06-21 16:08:27 +0300

    added ducks pattern & renamed delete method

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React, {useEffect, useState} from 'react';
 import ReactDOM from 'react-dom';
-import {initiateStore} from "./store/store";
-import * as actions from './store/actions'
+import configureStore from "./store/store";
+import {taskCompleted,titleChanged, taskDeleted} from './store/task'
 
 
 
-const store = initiateStore()
+const store = configureStore()
 
 
 const App = () => {
@@ -18,15 +18,15 @@ const App = () => {
     }, [])
 
     const completeTask = (taskId) => {
-        store.dispatch(actions.taskCompleted(taskId))
+        store.dispatch(taskCompleted(taskId))
     }
 
     const changeTitle = (taskId) => {
-        store.dispatch(actions.titleChanged(taskId))
+        store.dispatch(titleChanged(taskId))
     }
 
-    const deleteTitle = (taskId) => {
-        store.dispatch(actions.titleDeleted(taskId))
+    const deleteTask = (taskId) => {
+        store.dispatch(taskDeleted(taskId))
     }
 
 
@@ -40,7 +40,7 @@ const App = () => {
                     <p>{`Completed: ${el.completed}`}</p>
                     <button onClick={() => completeTask(el.id)}>completed</button>
                     <button onClick={() => changeTitle(el.id)}>Change title</button>
-                    <button onClick={() => deleteTitle(el.id)}>Delete task</button>
+                    <button onClick={() => deleteTask(el.id)}>Delete task</button>
                     <hr/>
                 </li>)}
             </ul>
--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,13 @@
 import {createStore} from "redux";
-import {taskReducer} from "./taskReducer";
+import reducer from "./task";
 
 const initialState = [
     {id: 1, title: "Task 1", completed: false},
     {id: 2, title: "Task 2", completed: false}
 ]
 
-export function initiateStore() {
-    return createStore(taskReducer, initialState)
+function configureStore() {
+    return createStore(reducer, initialState)
 }
+
+export default configureStore
